b6628edf20b2dff3d15bca2638c26e2aaf40b09f
naomi <orest.chupa@gmail.com>
2026-04-22 22:51:16 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,2 +1,55 @@
 class Dictionary:
-    pass
+    def __init__(
+        self,
+        initial_capacity: int = 8, 
+        load_factor: float | int = 2/3,
+    ) -> None:
+        
+        self.capacity = initial_capacity
+        self.load_factor = load_factor
+        self.size = 0
+
+        self.table = [None] * self.capacity
+    
+    def find_index(self, key: str) -> int:
+        return hash(key) % self.capacity
+    
+    def resize(self):
+        old_table = self.table
+        self.capacity *= 2
+        self.table = [None] * self.capacity
+        self.size = 0
+
+        for item in old_table:
+            if item is not None:
+                key, value = item
+                self[key] = value
+    
+    def __len__(self):
+        return self.size
+
+    def __getitem__(self, key):
+        index = self.find_index(key)
+
+        while self.table[index] is not None:
+            if self.table[index][0] == key:
+                return self.table[index][1]
+            index = (index + 1) % self.capacity
+        
+        raise KeyError
+    
+    def __setitem__(self, key, value):
+        if self.size >= self.capacity * self.load_factor:
+            self.resize()
+
+        index = self.find_index(key)
+
+        while self.table[index] is not None:
+            if self.table[index][0] == key:
+                break
+            index = (index + 1) % self.capacity
+
+        if self.table[index] is None:
+            self.size += 1
+
+        self.table[index] = (key, value)
