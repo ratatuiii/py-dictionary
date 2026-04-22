b50517884439a7d7634612c71e4ff731a1c1e7ce
naomi <orest.chupa@gmail.com>
2026-04-22 22:57:34 +0300

    PEP8 fix

--- app/main.py
+++ app/main.py
@@ -1,20 +1,23 @@
+from typing import Any
+
+
 class Dictionary:
     def __init__(
         self,
-        initial_capacity: int = 8, 
-        load_factor: float | int = 2/3,
+        initial_capacity: int = 8,
+        load_factor: float | int = 2 / 3,
     ) -> None:
-        
+
         self.capacity = initial_capacity
         self.load_factor = load_factor
         self.size = 0
 
         self.table = [None] * self.capacity
-    
+
     def find_index(self, key: str) -> int:
         return hash(key) % self.capacity
-    
-    def resize(self):
+
+    def resize(self) -> None:
         old_table = self.table
         self.capacity *= 2
         self.table = [None] * self.capacity
@@ -24,21 +27,21 @@ class Dictionary:
             if item is not None:
                 key, value = item
                 self[key] = value
-    
-    def __len__(self):
+
+    def __len__(self) -> int:
         return self.size
 
-    def __getitem__(self, key):
+    def __getitem__(self, key: Any) -> Any:
         index = self.find_index(key)
 
         while self.table[index] is not None:
             if self.table[index][0] == key:
                 return self.table[index][1]
             index = (index + 1) % self.capacity
-        
+
         raise KeyError
-    
-    def __setitem__(self, key, value):
+
+    def __setitem__(self, key: Any, value: Any) -> None:
         if self.size >= self.capacity * self.load_factor:
             self.resize()
 
